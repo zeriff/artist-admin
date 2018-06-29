40afa29f572922035533a8088e4dd20d95e4a512
sujanthakare <sujandt@gmail.com>
2018-06-29 23:03:37 +0530

    added Heroku url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { SigninComponent } from "./auth/signin/signin.component";
 import { TokenInterceptor } from "./auth/token.interceptor";
 import { ErrorsHandler } from "./providers/errors.handler";
 
-const BASE_URL = "https://f225e982.ngrok.io";
+const BASE_URL = "https://stark-everglades-32369.herokuapp.com";
 
 const appRoutes: Routes = [
   { path: "", component: DashboardComponent },
